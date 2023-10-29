ce0b107397b935e5b2d5fbe2feb40fc1573ac619
haidarfaizul <haidarfaizul7@gmail.com>
2023-10-29 19:16:39 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.lang.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean ulang = true;
+
+        do {
+            // Tampilkan menu
+            System.out.println("konversi satuan waktu");
+            System.out.println("menu:");
+            System.out.println("1. Menit -> Jam");
+            System.out.println("2. Jam -> menit");
+
+            // Ambil input pilihan
+            Scanner pilih = new Scanner(System.in);
+            int choose = pilih.nextInt();
+
+            // Proses konversi
+            switch (choose) {
+                case 1:
+                    // Konversi menit ke jam
+                    System.out.print("Masukan input menit: ");
+                    double menit = pilih.nextInt();
+                    double hasil1 = menit / 60;
+                    System.out.println("Hasil Konversi dari Menit ke Jam ialah: " + hasil1);
+                    break;
+
+                case 2:
+                    // Konversi jam ke menit
+                    System.out.print("Masukan input jam: ");
+                    double jam = pilih.nextInt();
+                    double hasil2 = jam * 60;
+                    System.out.println("Hasil Konversi dari Jam ke Menit ialah: " + hasil2);
+                    break;
+            }
+
+            // Tanyakan apakah ingin mengulang
+            System.out.print("Apakah anda ingin mengkonversi ulang?(y/n): ");
+            Scanner scanner = new Scanner(System.in);
+            char ulangChar = scanner.next().charAt(0);
+            ulang = ulangChar == 'y' || ulangChar == 'Y';
+        } while (ulang);
+    }
+}
